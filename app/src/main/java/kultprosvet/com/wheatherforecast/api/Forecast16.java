1326afd4f6be77aaa09985319c57ff9c30475f01
Stanislav Volnjanskij <stas@kultprosvet.net>
2016-07-14 18:56:27 +0300

    added forecast 16 objects

---
+++ app/src/main/java/kultprosvet/com/wheatherforecast/api/Forecast16.java
@@ -0,0 +1,117 @@
+
+package kultprosvet.com.wheatherforecast.api;
+
+import java.util.ArrayList;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Forecast16 {
+
+    @SerializedName("city")
+    @Expose
+    private City city;
+    @SerializedName("cod")
+    @Expose
+    private String cod;
+    @SerializedName("message")
+    @Expose
+    private Double message;
+    @SerializedName("cnt")
+    @Expose
+    private Integer cnt;
+    @SerializedName("list")
+    @Expose
+    private java.util.List<ForecastItem> forecastItem = new ArrayList<ForecastItem>();
+
+    /**
+     * 
+     * @return
+     *     The city
+     */
+    public City getCity() {
+        return city;
+    }
+
+    /**
+     * 
+     * @param city
+     *     The city
+     */
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    /**
+     * 
+     * @return
+     *     The cod
+     */
+    public String getCod() {
+        return cod;
+    }
+
+    /**
+     * 
+     * @param cod
+     *     The cod
+     */
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    /**
+     * 
+     * @return
+     *     The message
+     */
+    public Double getMessage() {
+        return message;
+    }
+
+    /**
+     * 
+     * @param message
+     *     The message
+     */
+    public void setMessage(Double message) {
+        this.message = message;
+    }
+
+    /**
+     * 
+     * @return
+     *     The cnt
+     */
+    public Integer getCnt() {
+        return cnt;
+    }
+
+    /**
+     * 
+     * @param cnt
+     *     The cnt
+     */
+    public void setCnt(Integer cnt) {
+        this.cnt = cnt;
+    }
+
+    /**
+     * 
+     * @return
+     *     The list
+     */
+    public java.util.List<ForecastItem> getForecastItem() {
+        return forecastItem;
+    }
+
+    /**
+     * 
+     * @param forecastItem
+     *     The list
+     */
+    public void setForecastItem(java.util.List<ForecastItem> forecastItem) {
+        this.forecastItem = forecastItem;
+    }
+
+}
