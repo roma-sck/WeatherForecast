8c36ddd9ecc7283dde53ddab33e97e8b9d834953
roma_sck <roma.sck@gmail.com>
2016-07-19 15:17:43 +0300

    added list item weather status

--- app/src/main/java/kultprosvet/com/wheatherforecast/api/OpenWeatherApi.java
+++ app/src/main/java/kultprosvet/com/wheatherforecast/api/OpenWeatherApi.java
@@ -6,14 +6,14 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-/**
- * Created by Stanislav Volnyansky on 12.07.16.
- */
 public interface OpenWeatherApi {
+
     @GET("weather")
     Call<TodayForecast> getTodayForecast(@Query("q") String q, @Query("units") String units, @Query("APPID") String appid);
-    @GET("weather")
-    Call<JsonElement> getTodayForecastJson(@Query("q") String q, @Query("units") String units, @Query("APPID") String appid);
+
     @GET("forecast/daily")
     Call<Forecast16> getForecast16(@Query("q") String q, @Query("units") String units, @Query("APPID") String appid);
+
+//    @GET("weather")
+//    Call<JsonElement> getTodayForecastJson(@Query("q") String q, @Query("units") String units, @Query("APPID") String appid);
 }
