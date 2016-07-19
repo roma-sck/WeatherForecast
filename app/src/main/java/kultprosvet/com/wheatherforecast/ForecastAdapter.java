85e8a8b12fe789d1adee3708fc051c1044552cbe
roma_sck <roma.sck@gmail.com>
2016-07-19 14:28:14 +0300

    formatted forecast list, load default icon

--- app/src/main/java/kultprosvet/com/wheatherforecast/ForecastAdapter.java
+++ app/src/main/java/kultprosvet/com/wheatherforecast/ForecastAdapter.java
@@ -1,11 +1,15 @@
 package kultprosvet.com.wheatherforecast;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.databinding.tool.DataBindingBuilder;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -13,8 +17,7 @@ import kultprosvet.com.wheatherforecast.api.ForecastItem;
 import kultprosvet.com.wheatherforecast.databinding.RowForecastBinding;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Holder> {
-
-    List<ForecastItem> items;
+    private List<ForecastItem> items;
 
     @Override
     public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -32,7 +35,7 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Holder
 
     @Override
     public int getItemCount() {
-        return items==null?0:items.size();
+        return items==null ? 0 : items.size();
     }
 
     public ForecastAdapter setItems(List<ForecastItem> items) {
@@ -42,11 +45,14 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Holder
     }
 
     public static class Holder extends RecyclerView.ViewHolder{
-        ForecastItem item;
-        RowForecastBinding binding;
+        private ForecastItem item;
+        private RowForecastBinding binding;
+        private Context mContext;
+
         public Holder(View itemView) {
             super(itemView);
-            binding=DataBindingUtil.bind(itemView);
+            mContext = itemView.getContext();
+            binding = DataBindingUtil.bind(itemView);
         }
 
         public ForecastItem getItem() {
@@ -56,7 +62,17 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Holder
         public Holder setItem(ForecastItem item) {
             this.item = item;
             binding.setItem(item);
+            getRowIcon();
             return this;
+        }
+
+        private void getRowIcon() {
+            DisplayMetrics metrics = mContext.getResources().getDisplayMetrics();
+            int size = Math.round(metrics.density * 100);
+            Picasso.with(mContext).load(R.drawable.cloud)
+                    .resize(size, size)
+                    .centerInside()
+                    .into(binding.rowIcon);
         }
     }
 }
