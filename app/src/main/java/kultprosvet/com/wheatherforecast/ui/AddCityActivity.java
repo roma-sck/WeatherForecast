7df61c2e1c5858688d8448b75cd6c459a59121bd
roma.sck <roma.sck@gmail.com>
2016-07-31 16:09:41 +0300

    added VALIDATING entered city name

--- app/src/main/java/kultprosvet/com/wheatherforecast/ui/AddCityActivity.java
+++ app/src/main/java/kultprosvet/com/wheatherforecast/ui/AddCityActivity.java
@@ -1,10 +1,10 @@
 package kultprosvet.com.wheatherforecast.ui;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import kultprosvet.com.wheatherforecast.R;
 import kultprosvet.com.wheatherforecast.databinding.ActivityAddCityBinding;
@@ -13,6 +13,7 @@ import kultprosvet.com.wheatherforecast.db.CityDbDao;
 import kultprosvet.com.wheatherforecast.db.DBHelper;
 
 public class AddCityActivity extends AppCompatActivity {
+    private static final String CITY_NAME_CHECK_REGEX = "^[A-Z][a-z]+(?: [A-Z]\\.[A-Z]\\.?|(?: [A-Z][a-z]+)*)";
     private ActivityAddCityBinding binding;
 
     @Override
@@ -23,9 +24,29 @@ public class AddCityActivity extends AppCompatActivity {
 
     public void saveCityToDb(View view) {
         CityDbDao cityDao = DBHelper.getSession(this).getCityDbDao();
-        CityDb city = new CityDb();
-        city.setName(binding.enteredCityName.getText().toString());
-        cityDao.insert(city);
-        finish();
+
+        String enteredValue = binding.enteredCityName.getText().toString();
+        if(validateCity(enteredValue)) {
+            CityDb city = new CityDb();
+            city.setName(enteredValue);
+            cityDao.insert(city);
+            finish();
+        } else {
+            wrongInputMsg();
+        }
+    }
+
+    /**
+     * Simple validating entered city name
+     *
+     * @param cityName name
+     * @return true if checked
+     */
+    public boolean validateCity(String cityName) {
+        return cityName.matches(CITY_NAME_CHECK_REGEX);
+    }
+
+    public void wrongInputMsg() {
+        Toast.makeText(this, getString(R.string.wrong_city_name_msg_text), Toast.LENGTH_SHORT).show();
     }
 }
